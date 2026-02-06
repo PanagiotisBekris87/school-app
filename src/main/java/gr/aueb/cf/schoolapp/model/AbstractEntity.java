c8c6ffdd5476abe9c0ff3a8d26bc4b892694cf50
Panagiotis Bekris <pbekris8@gmail.com>
2026-02-06 15:08:15 +0200

    intializing schoolapp project

---
+++ src/main/java/gr/aueb/cf/schoolapp/model/AbstractEntity.java
@@ -0,0 +1,32 @@
+package gr.aueb.cf.schoolapp.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@MappedSuperclass                  // Δεν θα δημιουργηθεί entity, δεν θα γίνει πίνακας στην db είναι μόνο για να κληρονομηθεί
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public abstract class AbstractEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false, columnDefinition = "DATETIME")
+    private Instant createdAt;          // UTC
+
+
+    @Column(name = "updated_at", nullable = false, columnDefinition = "DATETIME")
+    private Instant updatedAt;
+
+
+}
