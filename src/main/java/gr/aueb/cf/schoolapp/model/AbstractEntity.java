c8e9b0b92dc978dc0b38e522b44539ae69c44520
Panagiotis Bekris <pbekris8@gmail.com>
2026-02-11 23:10:17 +0200

    fix: updated procedure in AbstractEntity

--- src/main/java/gr/aueb/cf/schoolapp/model/AbstractEntity.java
+++ src/main/java/gr/aueb/cf/schoolapp/model/AbstractEntity.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
@@ -24,7 +25,7 @@ public abstract class AbstractEntity {
     @Column(name = "created_at", nullable = false, updatable = false, columnDefinition = "DATETIME")
     private Instant createdAt;          // Το instant είναι μία στιγμή στο χρόνο και είναι πάντα UTC
 
-
+    @LastModifiedDate
     @Column(name = "updated_at", nullable = false, columnDefinition = "DATETIME")
     private Instant updatedAt;
 
